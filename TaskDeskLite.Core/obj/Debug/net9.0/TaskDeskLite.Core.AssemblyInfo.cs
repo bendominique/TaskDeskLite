3d72c56c735f534c315bea7a69ee88c9256c8804
benjamin.dmakangwa <benjamin.dmakangwa@TIT0676433W11-1>
2026-01-23 15:03:06 -0300

    beleza

--- TaskDeskLite.Core/obj/Debug/net9.0/TaskDeskLite.Core.AssemblyInfo.cs
+++ TaskDeskLite.Core/obj/Debug/net9.0/TaskDeskLite.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskDeskLite.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c5679387ba212e76134a860581673e964cfcd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63f5f427b301caab52d8514f897d505c7a12f24a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskDeskLite.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskDeskLite.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
